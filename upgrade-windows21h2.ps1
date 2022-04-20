a0b11329da5c93f4b91a1df7e7c17dd1bf304080
srnvs9kumar <33701062+srnvs9kumar@users.noreply.github.com>
2022-04-20 18:25:04 +0530

    Used to upgrade to win10 21H2 upgrade assistent

---
+++ upgrade-windows21h2.ps1
@@ -0,0 +1,8 @@
+$dir = 'C:\temp'
+if(-not (test-path $dir)){mkdir $dir }
+$webClient = New-Object System.Net.WebClient
+$url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
+$file = "$($dir)\Windows10Upgrade9252.exe"
+$webClient.DownloadFile($url,$file)
+Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /NoRestartUI /copylogs $dir'
+ 
