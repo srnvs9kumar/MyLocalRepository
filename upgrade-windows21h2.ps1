601d6bde45e4f68f4449d95a2fc6f998e1938e77
srnvs9kumar <33701062+srnvs9kumar@users.noreply.github.com>
2022-04-20 18:28:34 +0530

    removed quiet install key

--- upgrade-windows21h2.ps1
+++ upgrade-windows21h2.ps1
@@ -4,5 +4,5 @@ $webClient = New-Object System.Net.WebClient
 $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
 $file = "$($dir)\Windows10Upgrade9252.exe"
 $webClient.DownloadFile($url,$file)
-Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /NoRestartUI /copylogs $dir'
+Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /NoRestartUI /copylogs $dir'
  
